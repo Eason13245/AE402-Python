8c1898cc07e6ef71c658bb1a3df06091c77f5d8f
Eason13245 <65231444+Eason13245@users.noreply.github.com>
2020-10-27 19:27:08 +0900

    Homework 10/27 Collision of blocks

--- Hoemwork-collision of blocks.py
+++ Hoemwork-collision of blocks.py
@@ -1,52 +1,66 @@
-import pygame, random
+import pygame
+import random
 
-BLACK = (0, 0, 0)
+BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+RED   = (255,   0,   0)
 
 pygame.init()
 
 screen_width = 700
 screen_height = 400
-screen = pygame.display.set_mode([screen_width,screen_height])
+screen = pygame.display.set_mode([screen_width, screen_height])
 
 player_x = 0
 player_y = 0
-player_W = 50
+player_w = 50
 player_h = 50
 
-block_W = 50
+block_w = 50
 block_h = 50
-block_x = random.randrange(screen_width-block_W)
-block_y = random.randrange(screen_height-block_h)
 
-collision = False
+block_x =[]
+block_y =[]
+collision =[]
+for i in range(10):
+    block_x.append(random.randrange(screen_width))
+    block_y.append(random.randrange(screen_height))
+    collision.append(False)
+
+
+font = pygame.font.Font(None,50)
+done = False
 score = 0
-done =False
 clock = pygame.time.Clock()
 
 while not done:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            done = True
-
-    xin = block_x<=player_x<=block_x+block_W or block_x<=player_x+player_W<=block_x+block_W
-    yin = block_y<=player_y<=block_y+block_h or block_y<=player_y+player_h<=block_y+block_h
-    if xin and yin and not collision:
-        collision = True
-
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+                done = True 
+    for i in range(10):            
+        xin = block_x[i]<=player_x<=block_x[i]+block_w or block_x[i]<=player_x+player_w<=block_x[i]+block_w
+        yin = block_y[i]<=player_y<=block_y[i]+block_h or block_y[i]<=player_y+player_h<=block_y[i]+block_h
+        if  xin and yin and not collision[i]:
+            collision[i] = True
+            score += 1
+        
     screen.fill(WHITE)
 
     pos = pygame.mouse.get_pos()
+    
+    player_x = pos[0]
+    player_y = pos[1]
+    pygame.draw.rect(screen,RED,[player_x,player_y,player_w,player_h])
 
-    pygame.draw.rect(screen, RED, [player_x, player_y, player_W, player_h])
-    pygame.draw.rect(screen, BLACK, [block_x, block_y, block_W, block_h])
+    for i in range(10):
+        if not collision[i]:
+            pygame.draw.rect(screen,BLACK,[block_x[i],block_y[i],block_w,block_h])        
 
-    if not xin and yin:
-        pygame.draw.rect(screen, RED, pos, [player_W, player_h])
-        pygame.display.flip()
-
+    message = " You got "+ str(score)+" points!"
+    text = font.render(message, True, BLACK)
+    screen.blit(text, (10,10))
+    pygame.display.flip()
     clock.tick(60)
 
-pygame.quit
+pygame.quit()
 
